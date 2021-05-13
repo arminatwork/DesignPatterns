fc27d03b3227a99f2929c59b25d260407f983c20
arminatwork <workwitharmin7@gmail.com>
2021-05-13 18:03:09 +0430

    feat added singleton creational pattern

---
+++ Creational/Singleton_Example/README.md
@@ -0,0 +1,9 @@
+﻿# Participants
+
+ The classes and objects participating in this pattern are:
+
+## [Singleton   (LoadBalancer)](https://github.com/arminatwork/DesignPatterns/tree/master/Creational/Singleton_Example/Singleton)
+
+ defines an Instance operation that lets clients access its unique instance. Instance is a class operation.
+
+ responsible for creating and maintaining its own unique instance.
---
+++ Creational/Singleton_Example/Singleton/LoadBalancer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Singleton_Example.Singleton
+{
+    /// <summary>
+    /// The 'Singleton' class
+    /// </summary>
+    class LoadBalancer
+    {
+        private static LoadBalancer _instance;
+        private readonly List<string> _servers = new();
+        private readonly Random _random = new();
+
+        // Lock synchronization object
+        private static readonly object SyncLock = new();
+
+        // Constructor (protected)
+        protected LoadBalancer()
+        {
+            // List of available servers
+            _servers.Add("Server Iran");
+            _servers.Add("Server Germany");
+            _servers.Add("Server English");
+            _servers.Add("Server Turkey");
+            _servers.Add("Server Russia");
+        }
+
+        public static LoadBalancer GetLoadBalancer()
+        {
+            // Support multi threaded applications through
+            // 'Double checked locking' pattern which (once
+            // the instance exists) avoids locking each
+            // time the method is invoked
+            if (_instance is null)
+            {
+                lock (SyncLock)
+                {
+                    _instance ??= new();
+                }
+            }
+
+            return _instance;
+        }
+
+        // Simple, but effective random load balancer
+        public string Server
+        {
+            get
+            {
+                int r = _random.Next(_servers.Count);
+                return _servers[r];
+            }
+        }
+    }
+}
